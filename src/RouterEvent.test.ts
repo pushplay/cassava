14b76e4b19c2cb7fe743df27a29e8abe196a6b7f
jeff <jeff@giftbit.com>
2017-11-03 14:55:51 -0700

    Move remaining header and queryStringParameter validation on to RouterEvent.

--- src/RouterEvent.test.ts
+++ src/RouterEvent.test.ts
@@ -3,7 +3,7 @@ import {RouterEvent} from "./RouterEvent";
 import {RestError} from "./RestError";
 
 describe("RouterEvent", () => {
-    describe("requireQueryParam()", () => {
+    describe("requireQueryStringParameter()", () => {
         const evt = new RouterEvent();
         evt.queryStringParameters = {
             a: "a",
@@ -46,6 +46,112 @@ describe("RouterEvent", () => {
             chai.assert.throws(() => {
                 evt.requireQueryStringParameter("c", () => true);
             }, RestError);
+        });
+    });
+
+    describe("requireHeader()", () => {
+        const evt = new RouterEvent();
+        evt.headers = {
+            A: "a",
+            B: "b"
+        };
+        evt._headersLowerCase = {
+            a: "a",
+            b: "b"
+        };
+
+        it("can require the header field is set", () => {
+            evt.requireHeader("a");
+            evt.requireHeader("A");
+            evt.requireHeader("b");
+            evt.requireHeader("B");
+            chai.assert.throws(() => {
+                evt.requireHeader("bee");
+            }, RestError);
+            chai.assert.throws(() => {
+                evt.requireHeader("c");
+            }, RestError);
+        });
+
+        it("can require the header field is one of a set of values", () => {
+            evt.requireHeader("a", ["a", "alpha", "aleph"]);
+            chai.assert.throws(() => {
+                evt.requireHeader("b", []);
+            }, RestError);
+            chai.assert.throws(() => {
+                evt.requireHeader("b", ["beta"]);
+            }, RestError);
+            chai.assert.throws(() => {
+                evt.requireHeader("c", ["c", "charlie"]);
+            }, RestError);
+        });
+
+        it("can require the header field validates against a function", () => {
+            evt.requireHeader("a", () => true);
+            evt.requireHeader("a", a => a === "a");
+            chai.assert.throws(() => {
+                evt.requireHeader("b", () => false);
+            }, RestError);
+            chai.assert.throws(() => {
+                evt.requireHeader("b", b => b !== "b");
+            }, RestError);
+            chai.assert.throws(() => {
+                evt.requireHeader("c", () => true);
+            }, RestError);
+        });
+    });
+
+    describe("blacklistQueryStringParameters()", () => {
+        const evt = new RouterEvent();
+        evt.queryStringParameters = {
+            o: "oscar",
+            b: "bravo",
+            j: "juliette",
+            e: "echo",
+            c: "charlie",
+            t: "tango"
+        };
+
+        it("doesn't throw an error if there are 0 keys blacklisted", () => {
+            evt.blacklistQueryStringParameters();
+        });
+
+        it("doesn't throw an error if no keys are found", () => {
+            evt.blacklistQueryStringParameters("bl", "a", "ck");
+        });
+
+        it("throws an error if a key is found", () => {
+            chai.assert.throws(() => {
+                evt.blacklistQueryStringParameters("b", "l", "a", "c", "k");
+            });
+        });
+    });
+
+    describe("whitelistStringQueryParameters()", () => {
+        const evt = new RouterEvent();
+        evt.queryStringParameters = {
+            o: "oscar",
+            b: "bravo",
+            j: "juliette",
+            e: "echo",
+            c: "charlie",
+            t: "tango"
+        };
+
+        it("doesn't throw an error if all keys are whitelisted", () => {
+            const emptyEvt = new RouterEvent();
+            emptyEvt.whitelistStringQueryParameters();
+
+            evt.whitelistStringQueryParameters("o", "b", "j", "e", "c", "t");
+        });
+
+        it("throws an error if a key is not listed", () => {
+            chai.assert.throws(() => {
+                evt.whitelistStringQueryParameters();
+            });
+            chai.assert.throws(() => {
+                evt.whitelistStringQueryParameters("o", "b", "j");
+            });
         });
     });
 });
--- src/RouterEvent.ts
+++ src/RouterEvent.ts
@@ -83,18 +83,21 @@ export class RouterEvent {
 
     /**
      * Require that the given query parameter is set.
+     * If the parameter is not set a RestError is thrown.
      */
     requireQueryStringParameter(param: string): this;
 
     /**
      * Require that the given query parameter is set and has
-     * one of the given values.
+     * one of the given values.  If the parameter is not set or
+     * does not take on one of the given values a RestError is thrown.
      */
     requireQueryStringParameter(param: string, values: string[], explanation?: string): this;
 
     /**
      * Require that the given query parameter is set and satisfies
-     * the validator function.
+     * the validator function.  If the parameter is not set or
+     * does not satisfy the given validator function a RestError is thrown.
      */
     requireQueryStringParameter(param: string, validator: (value: string) => boolean, explanation?: string): this;
 
@@ -109,5 +112,70 @@ export class RouterEvent {
             throw new RestError(400, explanation || `Required query parameter '${param}=${this.queryStringParameters[param]}' is not a legal value.`);
         }
         return this;
+    }
+
+    /**
+     * Require that the given query parameters are *not* set.
+     * If any parameter in this list is set a RestError is thrown.
+     */
+    blacklistQueryStringParameters(...params: string[]): void {
+        blacklistKeys(this.queryStringParameters || {}, params, "query parameter");
+    }
+
+    /**
+     * Require that only the given query parameters are set.  No parameter
+     * in this list has to be set, but if any parameter is set that is not in
+     * this list a RestError will be thrown.
+     */
+    whitelistStringQueryParameters(...params: string[]): void {
+        whitelistKeys(this.queryStringParameters || {}, params, "query parameter");
+    }
+
+    /**
+     * Require that the given header field is set.
+     */
+    requireHeader(field: string): this;
+
+    /**
+     * Require that the given header field is set and has
+     * one of the given values.
+     */
+    requireHeader(field: string, values: string[], explanation?: string): this;
+
+    /**
+     * Require that the given header field is set and satisfies
+     * the validator function.
+     */
+    requireHeader(field: string, validator: (value: string) => boolean, explanation?: string): this;
+
+    requireHeader(field: string, valuesOrValidator?: string[] | ((value: string) => boolean), explanation?: string): this {
+        const fieldLowerCase = field.toLowerCase();
+
+        if (!this._headersLowerCase[fieldLowerCase]) {
+            throw new RestError(400, explanation || `Required header '${field}' is not set.`);
+        }
+        if (valuesOrValidator && Array.isArray(valuesOrValidator) && valuesOrValidator.indexOf(this._headersLowerCase[fieldLowerCase]) === -1) {
+            throw new RestError(400, explanation || `Required header '${field}=${this._headersLowerCase[fieldLowerCase]}' must be one of: ${valuesOrValidator.join(", ")}.`);
+        }
+        if (valuesOrValidator && typeof valuesOrValidator === "function" && !valuesOrValidator(this._headersLowerCase[fieldLowerCase])) {
+            throw new RestError(400, explanation || `Required header '${field}=${this._headersLowerCase[fieldLowerCase]}' is not a legal value.`);
+        }
+        return this;
+    }
+}
+
+function blacklistKeys(o: object, keys: string[], part: string = "member"): void {
+    for (const key of Object.keys(o)) {
+        if (keys.indexOf(key) !== -1) {
+            throw new RestError(400, `Unexpected ${part} '${key}'.`);
+        }
+    }
+}
+
+function whitelistKeys(o: object, keys: string[], part: string = "member"): void {
+    for (const key of Object.keys(o)) {
+        if (keys.indexOf(key) === -1) {
+            throw new RestError(400, `Unexpected ${part} '${key}'.`);
+        }
     }
 }
