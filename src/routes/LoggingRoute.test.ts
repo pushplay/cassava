b10a7c172a31224fbc57e1ef4ff9ee5ad5eb065b
jeff <jeff@giftbit.com>
2017-12-14 16:38:04 -0800

    Release 1.4.0.  Added configurable headers logging to LoggingRoute.

---
+++ src/routes/LoggingRoute.test.ts
@@ -0,0 +1,115 @@
+import * as chai from "chai";
+import * as cassava from "../index";
+import {LoggingRoute} from "./LoggingRoute";
+import {createTestProxyEvent, testRouter} from "../testing";
+
+describe("LoggingRoute", () => {
+
+    const happyRoute: cassava.routes.Route = {
+        matches: () => true,
+        handle: () => ({
+            body: "I am text",
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "text/plain",
+                "Date": "Thu, 10 Sept 1982 12:00:00 GMT"
+            }
+        })
+    };
+
+    // Logging to console.log is a pita to test and assumed to work.
+
+    it("can configure the logging function", async () => {
+        const msgs: string[] = [];
+        const router = new cassava.Router();
+        router.route(new LoggingRoute({
+            logFunction: msg => msgs.push(msg)
+        }));
+        router.route(happyRoute);
+
+        await testRouter(router, createTestProxyEvent("/"));
+        chai.assert.equal(msgs.length, 2);
+    });
+
+    it("can log all request headers", async () => {
+        const msgs: string[] = [];
+        const router = new cassava.Router();
+        router.route(new LoggingRoute({
+            logFunction: msg => msgs.push(msg),
+            logRequestHeaders: true
+        }));
+        router.route(happyRoute);
+
+        await testRouter(router, createTestProxyEvent("/", "GET", {
+            headers: {
+                Accept: "text/plain",
+                Host: "www.example.com",
+                Origin: "http://www.example.com"
+            }
+        }));
+        chai.assert.equal(msgs.length, 2);
+        chai.assert.include(msgs[0], "Accept=text/plain");
+        chai.assert.include(msgs[0], "Host=www.example.com");
+        chai.assert.include(msgs[0], "Origin=http://www.example.com");
+        chai.assert.include(msgs[1], "Accept=text/plain");
+        chai.assert.include(msgs[1], "Host=www.example.com");
+        chai.assert.include(msgs[1], "Origin=http://www.example.com");
+    });
+
+    it("can log a subset of request headers", async () => {
+        const msgs: string[] = [];
+        const router = new cassava.Router();
+        router.route(new LoggingRoute({
+            logFunction: msg => msgs.push(msg),
+            logRequestHeaders: ["Host", "Kwyjibo"]
+        }));
+        router.route(happyRoute);
+
+        await testRouter(router, createTestProxyEvent("/", "GET", {
+            headers: {
+                Accept: "text/plain",
+                Host: "www.example.com",
+                Origin: "http://www.example.com"
+            }
+        }));
+        chai.assert.equal(msgs.length, 2);
+        chai.assert.notInclude(msgs[0], "Accept=text/plain");
+        chai.assert.include(msgs[0], "Host=www.example.com");
+        chai.assert.notInclude(msgs[0], "Origin=http://www.example.com");
+        chai.assert.notInclude(msgs[1], "Accept=text/plain");
+        chai.assert.include(msgs[1], "Host=www.example.com");
+        chai.assert.notInclude(msgs[1], "Origin=http://www.example.com");
+    });
+
+    it("can log all response headers", async () => {
+        const msgs: string[] = [];
+        const router = new cassava.Router();
+        router.route(new LoggingRoute({
+            logFunction: msg => msgs.push(msg),
+            logResponseHeaders: true
+        }));
+        router.route(happyRoute);
+
+        await testRouter(router, createTestProxyEvent("/", "GET", {}));
+        chai.assert.equal(msgs.length, 2);
+        chai.assert.include(msgs[1], "Access-Control-Allow-Origin=*");
+        chai.assert.include(msgs[1], "Content-Type=text/plain");
+        chai.assert.include(msgs[1], "Date=Thu, 10 Sept 1982 12:00:00 GMT");
+    });
+
+    it("can log a subset of response headers", async () => {
+        const msgs: string[] = [];
+        const router = new cassava.Router();
+        router.route(new LoggingRoute({
+            logFunction: msg => msgs.push(msg),
+            logResponseHeaders: ["Content-Type", "Kwyjibo"]
+        }));
+        router.route(happyRoute);
+
+        await testRouter(router, createTestProxyEvent("/", "GET", {}));
+        chai.assert.equal(msgs.length, 2);
+        chai.assert.notInclude(msgs[1], "Access-Control-Allow-Origin=*");
+        chai.assert.include(msgs[1], "Content-Type=text/plain");
+        chai.assert.notInclude(msgs[1], "Date=Thu, 10 Sept 1982 12:00:00 GMT");
+    });
+});
--- src/routes/LoggingRoute.ts
+++ src/routes/LoggingRoute.ts
@@ -15,24 +15,35 @@ export class LoggingRoute implements Route {
     }
 
     handle(evt: RouterEvent): void {
-        let msg = `${evt.httpMethod} ${evt.path}${this.queryMapToString(evt.queryStringParameters)}`;
-        if (!this.options.hideRequestBody && evt.body != null) {
-            msg += ` reqbody=${JSON.stringify(evt.body)}`;
-        }
+        const msg = this.requestToString(evt);
         this.log(msg);
     }
 
     postProcess(evt: RouterEvent, resp: RouterResponse): void {
-        let msg = `${evt.httpMethod} ${evt.path}${this.queryMapToString(evt.queryStringParameters)}`;
+        const msg = `${this.requestToString(evt)} ${this.responseToString(resp)}`;
+        this.log(msg);
+    }
 
+    requestToString(evt: RouterEvent): string {
+        let msg = `${evt.httpMethod} ${evt.path}${this.queryMapToString(evt.queryStringParameters)}`;
         if (!this.options.hideRequestBody && evt.body != null) {
             msg += ` reqbody=${JSON.stringify(evt.body)}`;
         }
-        msg += ` status=${resp.statusCode || 200}`;
+        if (this.options.logRequestHeaders) {
+            msg += ` reqheaders={${this.headersToString(evt.headers, this.options.logRequestHeaders)}}`;
+        }
+        return msg;
+    }
+
+    responseToString(resp: RouterResponse): string {
+        let msg = `status=${resp.statusCode || 200}`;
         if (!this.options.hideResponseBody && resp.body != null) {
             msg += ` respbody=${JSON.stringify(resp.body)}`;
         }
-        this.log(msg);
+        if (this.options.logResponseHeaders) {
+            msg += ` respheaders={${this.headersToString(resp.headers, this.options.logResponseHeaders)}}`;
+        }
+        return msg;
     }
 
     queryMapToString(queryStringParameters: { [key: string]: string } | null): string {
@@ -46,6 +57,16 @@ export class LoggingRoute implements Route {
         return "?" + keys.map(key => `${encodeURIComponent(key)}=${encodeURIComponent(queryStringParameters[key])}`).join("&");
     }
 
+    headersToString(headers: { [key: string]: string }, headersFilter: true | string[]): string {
+        let keys = Object.keys(headers);
+        if (Array.isArray(headersFilter)) {
+            const filterLowerCase = headersFilter.map(h => h.toLowerCase());
+            keys = keys.filter(key => filterLowerCase ? filterLowerCase.indexOf(key.toLowerCase()) !== -1 : true);
+        }
+
+        return keys.map(key => `${key}=${headers[key]}`).join(", ");
+    }
+
     log(msg: string): void {
         if (this.options.logFunction) {
             this.options.logFunction(msg);
@@ -56,7 +77,28 @@ export class LoggingRoute implements Route {
 }
 
 export interface LoggingRouteOptions {
+    /**
+     * Whether to hide the body of the request from logs.
+     */
     hideRequestBody?: boolean;
+
+    /**
+     * Whether to hide the body of the response from logs.
+     */
     hideResponseBody?: boolean;
+
+    /**
+     * Function to log requests with (defaults to console.log).
+     */
     logFunction?: (msg: string) => void;
+
+    /**
+     * `true` to log all request headers or an array of the names of headers to log.
+     */
+    logRequestHeaders?: true | string[];
+
+    /**
+     * `true` to log all response headers or an array of the names of headers to log.
+     */
+    logResponseHeaders?: true | string[];
 }
